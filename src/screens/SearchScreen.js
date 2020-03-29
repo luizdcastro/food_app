c40eb46f7f6a068f1e502137884c94b8d4ca2c9d
Luiz Castro <lfernando.cwb@gmail.com>
2020-03-29 17:41:45 -0300

    return data to ResultsShow

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -22,11 +22,20 @@ const SearchScreen = () => {
                 onTermSubmit={() => searchApi()}
             />
             {errorMessage ? <Text>{errorMessage}</Text> : null}
-            
+
             <ScrollView>
-                <ResultsList results={filterResultsByPrice('$')} title="Cost Effective" />
-                <ResultsList results={filterResultsByPrice('$$')} title="Bit Pricier" />
-                <ResultsList results={filterResultsByPrice('$$$')} title="Big Spender" />
+                <ResultsList
+                    results={filterResultsByPrice('$')}
+                    title="Cost Effective"                
+                />
+                <ResultsList
+                    results={filterResultsByPrice('$$')}
+                    title="Bit Pricier"             
+                />
+                <ResultsList
+                    results={filterResultsByPrice('$$$')}
+                    title="Big Spender"                  
+                />
             </ScrollView>
         </>
     );
